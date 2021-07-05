a93035ed1f0c560c5e24c02a679b2b47d28c0320
Olalekan <odukoyaonline@gmail.com>
2021-07-05 07:59:11 +0100

    refactor

--- src/helpers/formatName.js
+++ src/helpers/formatName.js
@@ -2,7 +2,6 @@ const { capitalize } = require('./capitalize');
 
 const containerNameFormatter = (group) => (contName, descriptor) => {
   const name = `${capitalize(contName.split('.')[0])}${capitalize(group)}`;
-  // console.log('name', name, 'descriptor=-=-=', descriptor, '=-=-=-')
   return name;
 };
 

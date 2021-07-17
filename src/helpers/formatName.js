042d1b45f2d4bdd0f4b69ef4074c24523b12e3ca
Olalekan <odukoyaonline@gmail.com>
2021-07-17 10:48:54 +0100

    fix shell script

--- src/helpers/formatName.js
+++ src/helpers/formatName.js
@@ -1,7 +1,15 @@
 const { capitalize } = require('./capitalize');
 
 const containerNameFormatter = (group) => (contName) => {
-  const name = `${capitalize(contName.split('.')[0])}${capitalize(group)}`;
+  const sanitizedString = contName.replace(
+    new RegExp('(?:\\b|_)([a-z])', 'g'),
+    ($1) => $1.toUpperCase()
+  );
+
+  const cleanContName = sanitizedString.replace(new RegExp('[-]', 'g'), '');
+
+  const name = `${capitalize(cleanContName.split('.')[0])}${capitalize(group)}`;
+
   return name;
 };
 
